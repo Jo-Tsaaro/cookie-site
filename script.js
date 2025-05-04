4825de8b7fff74a2db68c49f7acc9aa205f8289d
Jo-Tsaaro <jotheeswaran@tsaaro.com>
2025-05-04 11:49:22 +0530

    cook 1

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+function setCookie(name, value, days) {
+  const expires = new Date(Date.now() + days * 86400000).toUTCString();
+  document.cookie = `${name}=${value}; expires=${expires}; path=/`;
+}
+
+function getCookie(name) {
+  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  return match ? match[2] : null;
+}
+
 function alertMsg() {
-    alert("Hello!");
+  const userVisited = getCookie("visited");
+  const messageBox = document.getElementById("message");
+
+  if (!userVisited) {
+    const msg = "Welcome to our NeuBrutalist site!";
+    alert(msg); // Optional popup
+    messageBox.textContent = msg;
+    setCookie("visited", "yes", 7); // Expires in 7 days
+  } else {
+    const msg = "Welcome back!";
+    messageBox.textContent = msg;
   }
-  
+}
